5902d990c609d963609657abf6aa3f9f61241899
Metzo-GH <Mouhamedhino@gmail.com>
2024-10-07 15:17:55 +0200

    Changing version java 23 to 17

--- student/src/main/java/atelier/student/student/Repository/StageRepository.java
+++ student/src/main/java/atelier/student/student/Repository/StageRepository.java
@@ -1,12 +1,8 @@
 package atelier.student.student.Repository;
 
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import atelier.student.student.Entity.Stage;
 
 public interface StageRepository extends CrudRepository<Stage, Long> {
-
-    List<Stage> findByTitre(String titre);
-
-    Stage findById(long id);   
+ 
 }
--- student/src/main/java/atelier/student/student/Repository/StudentRepository.java
+++ student/src/main/java/atelier/student/student/Repository/StudentRepository.java
@@ -1,13 +1,9 @@
 package atelier.student.student.Repository;
 
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import atelier.student.student.Entity.Student;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
 
-    List<Student> findByNom(String nom);
-
-    Student findById(long id);
     
 }
